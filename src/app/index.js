3d464855345c83241b22a143611e817668503861
Xander Griff <xandgriff@gmail.com>
2017-10-18 14:35:10 -0400

    Finish work on lesson 3

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-alert('it works');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Create component
+
+var ToDoComponent = React.createClass({
+	render: function(){
+		return(
+			<h1>Heloooo</h1>
+		);
+	}
+});
+
+ReactDOM.render(<ToDoComponent />, document.getElementById('todo-wrapper'));
